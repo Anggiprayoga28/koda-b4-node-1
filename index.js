04326e5fa4f4f5dc086434a6d202ffd6a17d635f
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-09-10 13:48:51 +0700

    Feat: Add file entry point

---
+++ index.js
@@ -0,0 +1,4 @@
+// const artisAsync = require('./lib/byArtisAsync');
+// const moveMusic = require('./lib/byArtisSync');
+const musicAsync = require('./lib/musicAsync')
+// const generatedMusic = require('./lib/musicSync');
